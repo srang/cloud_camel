2a253869166f44fd7f92dbe327783e23791bb4f0
Sam Rang <srang2010@gmail.com>
2016-10-28 10:55:17 -0500

    .

---
+++ src/main/java/org/test/cloud/sandbox/CloudCamelApplication.java
@@ -0,0 +1,12 @@
+package org.test.cloud.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCamelApplication.class, args);
+	}
+}
---
+++ src/test/java/org/test/cloud/sandbox/CloudCamelApplicationTests.java
@@ -0,0 +1,16 @@
+package org.test.cloud.sandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudCamelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
